08c22282585262ffe66038ba8e934d63ef0d91be
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-02 12:12:12 +0530

    express app initialized

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost:27017/industryMasters', {useNewUrlParser: true}).then(() => { // if connected successfully
     console.log('db connected ..'); 
+
+    const app = express(); // initializing our app
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: false}));
+    
+    app.listen(3000, () => {    // starting server
+        console.log('server started');
+    });
+    
+
 }).catch((e) => {   // else catch the error, i.e. exception handling
     console.log(e.toString());
 })
