d3d4db3f2d1f9772bd11c7af2d951b83728e2640
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-02 12:36:19 +0530

    connected to mongo cluster

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost:27017/industryMasters', {useNewUrlParser: true}).then(() => { // if connected successfully
+
+const uri = "mongodb+srv://sd:sdoshi983@cluster0.0nb30.mongodb.net/industryMasters?retryWrites=true&w=majority";
+
+mongoose.connect(uri, {useNewUrlParser: true}).then(() => { // if connected successfully
     console.log('db connected ..'); 
 
     const app = express(); // initializing our app
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: false}));
-    
+
     app.listen(3000, () => {    // starting server
         console.log('server started');
     });
