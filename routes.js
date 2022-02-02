efb2212aa7243cf5ecda44bafcbb3322f87cd815
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-02 13:42:07 +0530

    insert api for material master started

--- routes.js
+++ routes.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-const materialMaster = require('./models/materialMaster');
-const chemicalMaster = require('./models/chemicalMaster');
+const MaterialMaster = require('./models/materialMaster');
+const ChemicalMaster = require('./models/chemicalMaster');
 
+// material master
 router.get('/get/material', async (request, response) => {
-    const data = await materialMaster.find();
+    const data = await MaterialMaster.find();
     response.send(data);
 });
 
+router.post('/post/material', (request, response) => {
+    // const material = new MaterialMaster({
+    //     modelName: request.body.modelName,
+    //     gradeName: request.body.gradeName,
+    //     diameter: request.body.diameter
+    // });
+    // await material.save();
+    // response.send(material);
+    response.send("hello");
+})
+// --------
+
+// chemical master
 router.get('/get/chemical', async (request, response) => {
-    const data = await chemicalMaster.find();
+    const data = await ChemicalMaster.find();
     response.send(data);
 });
 
