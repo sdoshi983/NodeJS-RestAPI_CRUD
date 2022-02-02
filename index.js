375c5f77b74fe70ec3ae969c0c88830a2946410c
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-02 12:10:12 +0530

    connected to mongodb database using mongoose

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost:27017/industryMasters', {useNewUrlParser: true}).then(() => { // if connected successfully
+    console.log('db connected ..'); 
+})
