31c1fa6705fc2c1114007ae8f364e33886f38959
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-02 12:55:17 +0530

    get api for chemical master created

--- routes.js
+++ routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+
 const materialMaster = require('./models/materialMaster');
+const chemicalMaster = require('./models/chemicalMaster');
 
 router.get('/get/material', async (request, response) => {
     const data = await materialMaster.find();
+    response.send(data);
+});
+
+router.get('/get/chemical', async (request, response) => {
+    const data = await chemicalMaster.find();
     response.send(data);
 });
 
